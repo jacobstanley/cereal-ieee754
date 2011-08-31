3f618d17ac4537d33a46edb82d527aa3ea02c871
Jacob Stanley <jacob@stanley.io>
2011-09-01 02:09:52 +0800

    Nitpick tweaks to documentation formatting

--- src/Data/Serialize/IEEE754.hs
+++ src/Data/Serialize/IEEE754.hs
@@ -50,54 +50,54 @@ import GHC.ST (runST, ST)
 ------------------------------------------------------------------------
 -- Get
 
--- | Read a 32-bit float in little endian format
+-- | Read a 32-bit float in little endian format.
 getFloat32le :: Get Float
 getFloat32le = wordToFloat <$> getWord32le
 
--- | Read a 64-bit float in little endian format
+-- | Read a 64-bit float in little endian format.
 getFloat64le :: Get Double
 getFloat64le = wordToDouble <$> getWord64le
 
--- | Read a 32-bit float in big endian format
+-- | Read a 32-bit float in big endian format.
 getFloat32be :: Get Float
 getFloat32be = wordToFloat <$> getWord32be
 
--- | Read a 64-bit float in big endian format
+-- | Read a 64-bit float in big endian format.
 getFloat64be :: Get Double
 getFloat64be = wordToDouble <$> getWord64be
 
--- | Read a 32-bit float in native host order and host endianness
+-- | Read a 32-bit float in native host order and host endianness.
 getFloat32host :: Get Float
 getFloat32host = wordToFloat <$> getWord32host
 
--- | Read a 64-bit float in native host order and host endianness
+-- | Read a 64-bit float in native host order and host endianness.
 getFloat64host :: Get Double
 getFloat64host = wordToDouble <$> getWord64host
 
 ------------------------------------------------------------------------
 -- Put
 
--- | Write a 32-bit float in little endian format
+-- | Write a 32-bit float in little endian format.
 putFloat32le :: Putter Float
 putFloat32le = putWord32le . floatToWord
 
--- | Write a 64-bit float in little endian format
+-- | Write a 64-bit float in little endian format.
 putFloat64le :: Putter Double
 putFloat64le = putWord64le . doubleToWord
 
--- | Write a 32-bit float in big endian format
+-- | Write a 32-bit float in big endian format.
 putFloat32be :: Putter Float
 putFloat32be = putWord32be . floatToWord
 
--- | Write a 64-bit float in big endian format
+-- | Write a 64-bit float in big endian format.
 putFloat64be :: Putter Double
 putFloat64be = putWord64be . doubleToWord
 
--- | Write a 32-bit float in native host order and host endianness
+-- | Write a 32-bit float in native host order and host endianness.
 putFloat32host :: Putter Float
 putFloat32host = putWord32host . floatToWord
 
--- | Write a 64-bit float in native host order and host endianness
+-- | Write a 64-bit float in native host order and host endianness.
 putFloat64host :: Putter Double
 putFloat64host = putWord64host . doubleToWord
 
